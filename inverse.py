ce5462f1337318aa25a99dd8f5c28a6af231670f
unknown <xxx-infiniti@yandex.ru>
2025-10-21 22:58:04 +0300

    Solution

---
+++ inverse.py
@@ -0,0 +1,9 @@
+# Пример с числом 4
+number = 4
+inverse_value = 1 / number
+print(f"Обратная величина для {number} равна {inverse_value}")
+
+# Пример с числом 0.5
+number = 0.5
+inverse_value = 1 / number
+print(f"Обратная величина для {number} равна {inverse_value}")
